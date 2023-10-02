008fe4531896fcaadba87a66031c46dd491d7514
daslu <daniel.slutsky@gmail.com>
2023-10-02 23:34:47 +0300

    - adapting to kindly v4 - adapting to changes in clerk - taking not only value but also form into account

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [1-alpha4-SNAPSHOT]
+- adapting to kindly v4
+- adapting to changes in clerk
+- taking not only value but also form into account
+
 ## [1-alpha3] - 2022-04-02
 - using fully-qualified symbols in render-fn
 
--- src/scicloj/kind_clerk/impl.clj
+++ src/scicloj/kind_clerk/impl.clj
@@ -1,17 +1,16 @@
 (ns scicloj.kind-clerk.impl
-  (:require [scicloj.kindly.v3.api :as kindly]
-            [scicloj.kindly.v3.kind :as kind]
+  (:require [scicloj.kindly.v4.api :as kindly]
+            [scicloj.kindly.v4.kind :as kind]
+            [scicloj.kindly-advice.v1.api :as kindly-advice]
             [nextjournal.clerk :as clerk]
             [nextjournal.clerk.viewer :as clerk.viewer]
             [clojure.string :as string]
             [clojure.pprint :as pprint]
             [scicloj.kind-clerk.viewers :as viewers]))
 
-
 (defn value->kind [v]
   (-> {:value v}
-      kindly/advice
-      last
+      kindly-advice/advise
       :kind))
 
 (def *kinds-to-ignore (atom #{}))
@@ -25,24 +24,38 @@
 (defn add-kind-transform! [kind transform]
   (swap! *kind->transform assoc kind transform))
 
+(let [pred (fn [v]
+             (when-let [k (value->kind v)]
+               (-> k
+                   (@*kinds-to-ignore)
+                   not)))]
+  (pred (+ 1 2)))
+
+(defn extract-context [v]
+  (kindly-advice/advise
+   {:form (:form v)
+    :value (-> v :nextjournal.clerk.viewer/result :nextjournal/value)}))
+
 (defn add-relevant-viewer! []
   (clerk/add-viewers!
    [{:pred (fn [v]
-             (when-let [k (value->kind v)]
+             (when-let [k (-> v
+                              extract-context
+                              :kind)]
                (-> k
                    (@*kinds-to-ignore)
                    not)))
      :transform-fn (clerk/update-val
                     (fn [v]
-                      (let [kind (value->kind v)]
+                      (let [{:keys [kind value]} (extract-context v)]
                         (if-let [transform (@*kind->transform kind)]
-                          (transform v)
+                          (transform value)
                           (clerk/html [:p "Unsupported kind "
                                        [:code (pr-str kind)]
                                        "."])))))}]))
 
 
-(add-kind-to-ignore! :kind/buffered-image)
+(add-kind-to-ignore! :kind/image)
 
 (add-kind-transform!
  :kind/vega-lite clerk/vl)
@@ -53,73 +66,75 @@
 (add-kind-transform!
  :kind/hiccup clerk/html)
 
-(add-kind-transform!
- :kind/md (fn [v]
-            (if (sequential? v)
-              (->> v
-                   (string/join "\n")
-                   clerk/md)
-              (-> v
-                  str
-                  clerk/md))))
 
-(add-kind-transform!
- :kind/dataset (fn [v]
-                 (clerk/html
-                  [:code {:class :scicloj-dataset}
-                   [:style "
-.scicloj-dataset th {
-  padding: 2px;
-}
-.scicloj-dataset td {
-  padding: 2px;
-}
-.scicloj-dataset th {
-  background-color: #ddd;
-}
-.scicloj-dataset tr:nth-child(even) {
-  background-color: #f6f6f6;
-}
-"]
-                   (-> v
-                       pprint/pprint
-                       with-out-str
-                       clerk/md)])))
 
-(add-kind-transform!
- :kind/table
- (fn [dataset-or-options]
-   (if (-> dataset-or-options
-           class
-           str
-           (= "class tech.v3.dataset.impl.dataset.Dataset"))
-     (nextjournal.clerk/table
-      {:head (-> dataset-or-options keys)
-       :rows (->> dataset-or-options
-                  vals
-                  (apply map vector))})
-     (let [{:keys [row-maps row-vectors column-names]} dataset-or-options]
-       (assert column-names)
-       (if row-vectors
-         (nextjournal.clerk/table
-          {:head column-names
-           :rows row-vectors})
-         (do
-           (assert row-maps)
-           (nextjournal.clerk/table
-            {:head column-names
-             :rows (->> row-maps
-                        (map #(->> column-names
-                                   (map %))))})))))))
+;; (add-kind-transform!
+;;  :kind/md (fn [v]
+;;             (if (sequential? v)
+;;               (->> v
+;;                    (string/join "\n")
+;;                    clerk/md)
+;;               (-> v
+;;                   str
+;;                   clerk/md))))
 
-(add-kind-transform!
- :kind/cytoscape (fn [v]
-                   (clerk.viewer/with-viewer
-                     viewers/cytoscape-viewer
-                     v)))
+;; (add-kind-transform!
+;;  :kind/dataset (fn [v]
+;;                  (clerk/html
+;;                   [:code {:class :scicloj-dataset}
+;;                    [:style "
+;; .scicloj-dataset th {
+;;   padding: 2px;
+;; }
+;; .scicloj-dataset td {
+;;   padding: 2px;
+;; }
+;; .scicloj-dataset th {
+;;   background-color: #ddd;
+;; }
+;; .scicloj-dataset tr:nth-child(even) {
+;;   background-color: #f6f6f6;
+;; }
+;; "]
+;;                    (-> v
+;;                        pprint/pprint
+;;                        with-out-str
+;;                        clerk/md)])))
 
-(add-kind-transform!
- :kind/echarts (fn [v]
-                 (clerk.viewer/with-viewer
-                   viewers/echarts-viewer
-                   v)))
+;; (add-kind-transform!
+;;  :kind/table
+;;  (fn [dataset-or-options]
+;;    (if (-> dataset-or-options
+;;            class
+;;            str
+;;            (= "class tech.v3.dataset.impl.dataset.Dataset"))
+;;      (nextjournal.clerk/table
+;;       {:head (-> dataset-or-options keys)
+;;        :rows (->> dataset-or-options
+;;                   vals
+;;                   (apply map vector))})
+;;      (let [{:keys [row-maps row-vectors column-names]} dataset-or-options]
+;;        (assert column-names)
+;;        (if row-vectors
+;;          (nextjournal.clerk/table
+;;           {:head column-names
+;;            :rows row-vectors})
+;;          (do
+;;            (assert row-maps)
+;;            (nextjournal.clerk/table
+;;             {:head column-names
+;;              :rows (->> row-maps
+;;                         (map #(->> column-names
+;;                                    (map %))))})))))))
+
+;; (add-kind-transform!
+;;  :kind/cytoscape (fn [v]
+;;                    (clerk.viewer/with-viewer
+;;                      viewers/cytoscape-viewer
+;;                      v)))
+
+;; (add-kind-transform!
+;;  :kind/echarts (fn [v]
+;;                  (clerk.viewer/with-viewer
+;;                    viewers/echarts-viewer
+;;                    v)))

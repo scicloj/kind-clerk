818bdcd2fa6c5fdf18f0d713860953eb6586eb78
daslu <daniel.slutsky@gmail.com>
2023-10-03 14:08:32 +0300

    updated demo (WIP)

--- examples/example-project/notebooks/index.clj
+++ examples/example-project/notebooks/index.clj
@@ -13,6 +13,16 @@
 {:x "A"
  :y "B"}
 
+;; ## Images
+(import
+ java.net.URL)
+
+(def clj-img
+  (->  "https://clojure.org/images/clojure-logo-120b.png"
+       (java.net.URL.)
+       (javax.imageio.ImageIO/read)))
+
+clj-img
 
 ;; ## Hiccup
 
@@ -21,6 +31,11 @@
 
 ^:kind/hiccup
 [:big 1234]
+
+;; ### Nesting kinds in hiccup (WIP)
+
+(kind/hiccup
+ [:div])
 
 ;; ## Markdown
 
@@ -34,52 +49,46 @@
 
 ;; ## Vega-Lite
 
-(def vega-lite-example
-  {:data {:values [{:x 1 :y 2}
-                   {:x 2 :y 4}
-                   {:x 3 :y 9}
-                   {:x 4 :y 16}]}
-   :mark :point
-   :encoding {:x {:field :x :type :quantitative}
-              :y {:field :y :type :quantitative}}})
-
-(kind/vega-lite vega-lite-example)
+(kind/vega-lite
+ {:data {:values [{:x 1 :y 2}
+                  {:x 2 :y 4}
+                  {:x 3 :y 9}
+                  {:x 4 :y 16}]}
+  :mark :point
+  :encoding {:x {:field :x :type :quantitative}
+             :y {:field :y :type :quantitative}}})
 
 ;; ## Cytoscape
 
-(def cytoscape-example
-  {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
-                      {:data {:id "b"}}
-                      {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
-                      {:data {:id "d"} :position {:x 215 :y 175}}
-                      {:data {:id "e"}}
-                      {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
-              :edges [{:data {:id "ad" :source "a" :target "d"}}
-                      {:data {:id "eb" :source "e" :target "b"}}]}
-   :style [{:selector "node"
-            :css {:content "data(id)"
-                  :text-valign "center"
-                  :text-halign "center"}}
-           {:selector "parent"
-            :css {:text-valign "top"
-                  :text-halign "center"}}
-           {:selector "edge"
-            :css {:curve-style "bezier"
-                  :target-arrow-shape "triangle"}}]
-   :layout {:name "preset"
-            :padding 5}})
-
-(kind/cytoscape cytoscape-example)
+(kind/cytoscape
+ {:elements {:nodes [{:data {:id "a" :parent "b"} :position {:x 215 :y 85}}
+                     {:data {:id "b"}}
+                     {:data {:id "c" :parent "b"} :position {:x 300 :y 85}}
+                     {:data {:id "d"} :position {:x 215 :y 175}}
+                     {:data {:id "e"}}
+                     {:data {:id "f" :parent "e"} :position {:x 300 :y 175}}]
+             :edges [{:data {:id "ad" :source "a" :target "d"}}
+                     {:data {:id "eb" :source "e" :target "b"}}]}
+  :style [{:selector "node"
+           :css {:content "data(id)"
+                 :text-valign "center"
+                 :text-halign "center"}}
+          {:selector "parent"
+           :css {:text-valign "top"
+                 :text-halign "center"}}
+          {:selector "edge"
+           :css {:curve-style "bezier"
+                 :target-arrow-shape "triangle"}}]
+  :layout {:name "preset"
+           :padding 5}})
 
 ;; ## Echarts
 
-(def echarts-example
-  {:xAxis {:data ["Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"]}
-   :yAxis {}
-   :series [{:type "bar"
-             :data [23 24 18 25 27 28 25]}]})
-
-(kind/echarts echarts-example)
+(kind/echarts
+ {:xAxis {:data ["Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"]}
+  :yAxis {}
+  :series [{:type "bar"
+            :data [23 24 18 25 27 28 25]}]})
 
 ;; ## Datasets
 
@@ -88,24 +97,35 @@
 (require '[tablecloth.api :as tc])
 
 (-> {:x [1 3 5]
-     :y [2 4 6]}
-    tc/dataset)
+     :y (repeatedly 3 rand)}
+    tc/dataset
+    (tc/set-dataset-name "my data"))
 
 ;; ## Tables
 
 ;; We can also use Clerk's table view:
 
-(-> {:x [1 3 5]
-     :y [2 4 6]}
+(-> {:x (range 100)
+     :y (repeatedly 100 rand)}
     tc/dataset
     kind/table)
 
 (-> {:column-names [:x :y]
-     :row-vectors (for [i (range 100)]
-                    [i (inc i)])}
+     :row-vectors (for [i (range 5)]
+                    [i (rand)])}
     kind/table)
 
 (-> {:column-names [:x :y]
-     :row-maps (for [i (range 100)]
-                 {:x i :y (inc i)})}
+     :row-maps (for [i (range 5)]
+                 {:x i :y (rand)})}
+    kind/table)
+
+;; ### Nesting kinds in tables (WIP)
+
+(-> {:column-names [:x :y]
+     :row-vectors [[1 (rand)]
+                   [2 [3 4]]
+                   [3 {:x 5
+                       :y 6}]
+                   [4 clj-img]]}
     kind/table)
